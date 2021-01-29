b80c7ce77f6420dee4beee7b5970e76867ce14cb
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-01-29 10:07:19 -0300

    Commit inicial

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,32 @@
+const { request } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/projects', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 3',
+        'Projeto 2'
+    ]);
+});
+
+app.delete('projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 3'
+    ]);
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started!');
+});
