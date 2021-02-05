5e8dac0d29d887750f0b024154f7c5e0345564f7
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-02-04 22:45:15 -0300

    Commit inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
 import Header from './components/Header';
 
 function App() {
@@ -11,6 +14,7 @@ function App() {
     return (
         <>
             <Header title="Projects" > 
+                <img width={300} src={backgroundImage} />
                 <ul>
                     {projects.map(project => <li key={project}>{project}</li>)}
                 </ul>
