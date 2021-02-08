f7586e7264fb29ebc3d18ae171ca959a864539a1
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-02-08 17:33:23 -0300

    Commit inicial

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { view } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-    return <View />;
+    return (
+    <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+        <View styles={styles.container} >
+            <Text style={styles.title}> Hello GoStack</Text> 
+        </View>
+    </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+    },
+
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold'
+    },
+});
